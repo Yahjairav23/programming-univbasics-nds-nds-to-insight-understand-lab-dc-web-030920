f057c33cb20fc7f84fd2b54699c9f61c52b14542
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-04 21:56:25 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,5 +11,11 @@ end
 
 def print_first_directors_movie_titles
     puts directors_database[0][:movies]
+  #loop through each row and and print the :title 
   
+  column_index = 0 
+  while column_index < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][column_index][:title]
+    column_index += 1 
+  end 
 end
