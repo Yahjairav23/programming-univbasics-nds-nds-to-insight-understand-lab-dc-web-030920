aacb41fc197775815bddfd832e2d58907e6b0d33
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-04 21:57:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-    puts directors_database[0][:movies]
+    
   #loop through each row and and print the :title 
   
   column_index = 0 
